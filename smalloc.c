e76441584a4a846e97e88feec7cd3303fe42c7ca
Lauro Weingartner Neto <laurowrn@gmail.com>
2023-06-12 12:02:44 -0300

    implementacao inicial sfree()

--- smalloc.c
+++ smalloc.c
@@ -13,6 +13,58 @@ struct mem_block {
 
 struct mem_block *mem_area;
 
+void print_single_mem_block(struct mem_block *m){
+    printf("\n");
+    printf("*****************************\n");
+    printf("****** Bloco escolhido ******\n");
+    printf("*****************************\n");    
+    printf("is_free: %d\n", m->is_free);
+    printf("size: %ld\n", m->size);
+    printf("mem_ptr: %p\n", m->mem_ptr);
+    printf("next: %p\n", m->next);
+    printf("*****************************\n");
+    printf("**-------------------------**\n");
+    printf("*****************************\n");
+}
+
+void print_mem_block(struct mem_block *m){
+    printf("\n");
+    printf("-----------------------------\n");
+    printf("------Comeco da memoria------\n");
+    printf("-----------------------------\n");    
+    while(m){
+        printf("is_free: %d\n", m->is_free);
+        printf("size: %ld\n", m->size);
+        printf("mem_ptr: %p\n", m->mem_ptr);
+        printf("next: %p\n", m->next);
+        printf("-----------------------------\n");
+        m = m->next;
+    }
+    printf("--------Fim da memoria-------\n");
+    printf("-----------------------------\n");  
+};
+
+void remove_mem_block(void* p) {
+    struct mem_block *targetBlock = (struct mem_block *) p;
+    struct mem_block *previousBlock = mem_area;
+    struct mem_block *laterBlock;
+    struct mem_block *currentBlock =  mem_area;
+    int index = 0;
+    while (currentBlock->mem_ptr != targetBlock->mem_ptr){
+        currentBlock = currentBlock->next;
+        index++;
+    }
+
+    for (int i = 0; i < index - 1; i++){
+        previousBlock = previousBlock->next;
+    }
+
+    laterBlock = currentBlock->next;
+
+    previousBlock->next = laterBlock;
+    free(currentBlock);
+}
+
 void* smalloc(size_t size) {
     struct mem_block *m = mem_area;
     struct mem_block *foundBlock = NULL;
@@ -25,7 +77,6 @@ void* smalloc(size_t size) {
         }
         m = m->next;
     }
-
 
     if(foundBlock){
         struct mem_block *newBlock = (struct mem_block *) malloc(sizeof(struct mem_block));
@@ -52,28 +103,41 @@ void* smalloc(size_t size) {
     }
 }
 
-
 void sfree(void* p) {
-
-}
-
-
-void print_mem_block(struct mem_block *m){
-    printf("\n");
-    printf("-----------------------------\n");
-    printf("------Comeco da memoria------\n");
-    printf("-----------------------------\n");    
-    while(m){
-        printf("is_free: %d\n", m->is_free);
-        printf("size: %ld\n", m->size);
-        printf("mem_ptr: %p\n", m->mem_ptr);
-        printf("next: %p\n", m->next);
-        printf("-----------------------------\n");
-        m = m->next;
+    struct mem_block *targetBlock = (struct mem_block *) p;
+    struct mem_block *previousBlock = mem_area;
+    struct mem_block *laterBlock;
+    struct mem_block *currentBlock =  mem_area;
+    int index = 0;
+    while (currentBlock->mem_ptr != targetBlock->mem_ptr){
+        currentBlock = currentBlock->next;
+        index++;
     }
-    printf("--------Fim da memoria-------\n");
-    printf("-----------------------------\n");  
-};
+
+    if (currentBlock->is_free == 0){
+        for (int i = 0; i < index - 1; i++){
+            previousBlock = previousBlock->next;
+        }
+
+        laterBlock = currentBlock->next;
+        int freeMemory = currentBlock->size;
+
+        if(previousBlock->is_free){
+            freeMemory += previousBlock->size;
+            remove_mem_block((void *) previousBlock);
+        }
+        if(laterBlock->is_free){
+            freeMemory += laterBlock->size;
+            remove_mem_block((void *) laterBlock);
+        }
+
+        currentBlock->is_free = 1;
+        currentBlock->size = freeMemory;
+
+    }else{
+        printf("O bloco já está livre!\n");
+    }
+}
 
 
 int main(){
@@ -83,28 +147,26 @@ int main(){
     mem_area->mem_ptr = (void *) mem_area;
     mem_area->next = NULL;
 
-    // struct mem_block *second_block = (struct mem_block *) malloc(sizeof(struct mem_block));
-    // second_block->is_free = 1;
-    // second_block->size = 8192;
-    // second_block->mem_ptr = (void *) malloc(sizeof(void));
-    // second_block->next = NULL;
+    print_mem_block(mem_area);
 
-    // mem_area->next = second_block;
+    struct mem_block *p1 = (struct mem_block *) smalloc(200);
+    struct mem_block *p2 = (struct mem_block *) smalloc(300);
+    struct mem_block *p3 = (struct mem_block *) smalloc(1000);
+    struct mem_block *p4 = (struct mem_block *) smalloc(5000);
 
     print_mem_block(mem_area);
 
-    smalloc(200);
-    smalloc(300);
-    smalloc(1000);
-    smalloc(5000);
-    smalloc(5000);
-
+    sfree((void *) p2);
     print_mem_block(mem_area);
 
-    // printf("%d", (int) sizeof(struct mem_block));
+    sfree((void *) p3);
+    print_mem_block(mem_area);
 
+    sfree((void *) p4);
+    print_mem_block(mem_area);
 
-    // print_mem_block(foundBlock);
+    sfree((void *) p1);
+    print_mem_block(mem_area);
 
     /* Exemplo de uso do smalloc(), sfree() e print_mem_block() */
     // char *p1 = (char*) smalloc(200);
